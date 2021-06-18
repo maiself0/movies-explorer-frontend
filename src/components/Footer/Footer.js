2bbd6f32ec7b1c6d6aea9c1a4008ec15349fe6b9
Stanislav Bukhgolts <sbuchholz@yandex.ru>
2021-06-18 18:52:26 +0300

    +: footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import './Footer.css';
 
+const date = new Date();
+
 const Footer = () => {
   return (
     <div className="footer">
-      
-    </div>
-  )
-}
+      <div className="footer__container">
+        <div className="footer__header-container">
+          <h3 className="footer__header">
+            Учебный проект Яндекс.Практикум х BeatFilm.
+          </h3>
+        </div>
 
-export default Footer
+        <div className="footer__content-container">
+          <p className="footer__date">© {date.getFullYear()}</p>
+
+          <ul className="footer__links">
+            <li className="footer__link">Яндекс.Практикум</li>
+            <li className="footer__link">Github</li>
+            <li className="footer__link">Facebook</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
